8b0238e4fae50792cf5a7c9292999444b98e7795
Kainã de Melo Ramos <kaina.ramos@outlook.com>
2024-04-26 16:46:13 -0400

    Adicionados novos estudos

--- 3. Variáveis/index.js
+++ 3. Variáveis/index.js
@@ -1,4 +1,6 @@
 let player_name = 'Xx_DarkDragon_xX'; // Declaração e inicialização da variável
+/*Também é possível declarar com o comando var, mas é menos usual por permitir
+sobrescrita de valores*/
 
 console.log(player_name, 'encontrou a jóia sagrada!');
 console.log(player_name, 'embuiu a jóia em sua adaga');
