4abf0aab736772074da5e1a1402d6e2c7255fd4b
Kainã de Melo Ramos <kaina.ramos@outlook.com>
2024-05-10 16:47:50 -0400

    Adicionado exercício IMC completo com HTML e CSS

---
+++ 13.Exercício IMC completo/index.js
@@ -0,0 +1,36 @@
+const peso = document.querySelector('#weight_input');
+const altura = document.querySelector('#height_input');
+
+const form = document.querySelector('#formulario_imc');
+const divResultado = document.querySelector('#resultado');
+
+function cancelaAtt (evento){
+    evento.preventDefault();
+
+    console.log(peso.value, altura.value);
+    const resultado = (peso.value / altura.value)/altura.value;
+   
+   if (isNaN(peso.value) === true && isNaN(altura.value) === true) {
+    divResultado.innerHTML = `<p id="invalid">Valores inválidos</p>`;  
+   } else if (isNaN(peso.value) === true){
+    divResultado.innerHTML = `<p id="invalid">Peso inválido</p>`; 
+   } else if (isNaN(altura.value) === true){
+    divResultado.innerHTML = `<p id="invalid">Altura inválida</p>`;  
+   } else if (resultado < 18.5) {
+    divResultado.innerHTML = `<p id="results">Abaixo do peso. (IMC: ${resultado.toFixed(1)})</p>`; 
+   } else if (resultado >= 18.5 && resultado <= 24.9) {
+    divResultado.innerHTML = `<p id="results">Peso normal. (IMC: ${resultado.toFixed(1)})</p>`; 
+   } else if (resultado >= 25 && resultado <= 29.9) {
+    divResultado.innerHTML = `<p id="results">Sobrepeso. (IMC: ${resultado.toFixed(1)})</p>`; 
+   } else if (resultado >= 30 && resultado <= 34.9) {
+    divResultado.innerHTML = `<p id="results">Obesidade grau I. (IMC: ${resultado.toFixed(1)})</p>`; 
+   } else if (resultado >= 35 && resultado <= 39.9) {
+    divResultado.innerHTML = `<p id="results">Obesidade grau II. (IMC: ${resultado.toFixed(1)})</p>`; 
+   } else if (resultado >= 40) {
+    divResultado.innerHTML = `<p id="results">Obesidade grau III. (IMC: ${resultado.toFixed(1)})</p>`; 
+   }
+};
+
+form.addEventListener ('submit', cancelaAtt);
+
+// imprimeResultado.innerHTML = ` <p id="results"> Seu IMC é: ${resultado.toFixed(1)} </p>`;
