996660a6909b525e0791947345d7e4001b97ef8f
Kainã de Melo Ramos <kaina.ramos@outlook.com>
2024-07-24 16:45:38 -0400

    Continuando exercício [Lista de Tarefas]

--- Exercícios/Lista de tarefas/script.js
+++ Exercícios/Lista de tarefas/script.js
@@ -1,4 +1,5 @@
-/* <li><p class="tarefa"></p></li> */
+// Refatorar para segmentar as funções ao longo do código
+// Assistir aula 67 a partir de 32' para aprender a salvar dados em JSON!!!
 
 const listaTarefa = document.getElementById('lista-tarefas');
 const formTarefa = document.querySelector('.form-tarefa');
@@ -7,15 +8,25 @@ formTarefa.addEventListener('submit', function(event){
     event.preventDefault();
     const inputTarefa = event.target.querySelector('.insercao-tarefa');
     const tarefa = inputTarefa.value;
+    
     if(inputTarefa.value === ''){
         alert('Insira uma tarefa.');
     } else {
         // Implementar: Remover placeholder ao inserir tarefa válida
-        let itemLista = document.createElement('li');
-        let pListItem = document.createElement('p');
+        const itemLista = document.createElement('li');
+        const pListItem = document.createElement('span');
+        const botaoApagar = document.createElement('button')
+        
+        botaoApagar.innerHTML = 'OK'
+        botaoApagar.classList.add('botao-concluir')
         pListItem.classList.add('tarefa');
         pListItem.innerHTML = tarefa;
+
         itemLista.appendChild(pListItem)
+        itemLista.appendChild(botaoApagar)
         listaTarefa.appendChild(itemLista)
+        
+        formTarefa.reset();
+        document.querySelector('.placeholder').innerHTML = '';
     }  
 })
