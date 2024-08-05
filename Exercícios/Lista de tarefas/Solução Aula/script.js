04ed937d287eceb1d4537e2aae7293ee10336007
Kainã <kaina.ramos@outlook.com>
2024-08-05 01:08:36 -0400

    Concluída solução aula para [Lista de tarefas]

--- Exercícios/Lista de tarefas/Solução Aula/script.js
+++ Exercícios/Lista de tarefas/Solução Aula/script.js
@@ -1,14 +1,87 @@
-// Refatorar para segmentar as funções ao longo do código
-// Assistir aula 67 a partir de 32' para aprender a salvar dados em JSON!!!
+// Constantes
 
-// const listaTarefa = document.getElementById('lista-tarefas');
-const formTarefa = document.querySelector('.form-tarefa');
-const inputTarefa = document.querySelector('.insercao-tarefa');
-const botaoAdicionar = document.querySelector('.adiciona-tarefa')
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
 
+// Functions
 
+function criaItem(){
+    const li = document.createElement('li');
+    return li;
+}
 
-botaoAdicionar.addEventListener('click', function() {
-    if (!inputTarefa.value) return;
-    console.log(inputTarefa.value);
-})
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaTarefa(inputText){
+    const novoItem = criaItem();
+    novoItem.innerText = inputText;
+    tarefas.appendChild(novoItem);
+    criaBotao(novoItem);
+    limpaInput();
+    armazenaTarefas();
+}
+
+function criaBotao(li){
+    li.innerText += ' ';
+    const botaoFeito = document.createElement('button');
+    botaoFeito.innerText = 'OK';
+    botaoFeito.setAttribute('class', 'apagar') //Define um atributo para o elemento em questão
+    li.appendChild(botaoFeito);
+}
+
+function armazenaTarefas(){
+    const listaElementos = tarefas.querySelectorAll('li');
+    const listaItens = [];
+
+    for(let item of listaElementos){
+        let textoDoItem = item.innerText;
+        textoDoItem = textoDoItem.replace('Feito', '').trim(); //Retira o texto do button e o espaço
+        listaItens.push(textoDoItem);
+    }
+    const listaJSON = JSON.stringify(listaItens);
+    localStorage.setItem('itensLista', listaJSON);
+}
+
+function salvarTarefas(){
+    const tarefasArmazenadas = localStorage.getItem('itensLista');
+    const listaTarefas = JSON.parse(tarefasArmazenadas);
+
+    for(let tarefa of listaTarefas){
+        criaTarefa(tarefa);
+    }
+    
+}
+
+salvarTarefas();
+// Event Listeners
+
+btnTarefa.addEventListener('click', function() {
+    if(!inputTarefa.value) {
+        alert('Insira uma tarefa válida');
+        return;
+    }
+    criaTarefa(inputTarefa.value);
+});
+
+inputTarefa.addEventListener('keypress', function(e){
+    if(e.keyCode === 13) {
+        if(!inputTarefa.value) {
+            alert('Insira uma tarefa válida');
+            return;
+        }
+        criaTarefa(inputTarefa.value)
+    };
+});
+
+document.addEventListener('click', function(e){
+    const clicado = e.target;
+   
+    if(clicado.classList.contains('apagar')){
+        clicado.parentElement.remove();
+        armazenaTarefas();
+    }
+});
