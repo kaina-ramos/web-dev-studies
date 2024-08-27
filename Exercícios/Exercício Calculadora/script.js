2642e28808fd5ffb6d1b129fff8cab308ad46102
Kainã de Melo Ramos <kaina.ramos@outlook.com>
2024-08-27 16:46:50 -0400

    Finished [Exercício Calculadora]

--- Exercícios/Exercício Calculadora/script.js
+++ Exercícios/Exercício Calculadora/script.js
@@ -6,14 +6,28 @@ function criaCalculadora(){
         
         // Métodos
         inicia(){
-            this.clickBotoes();
+            this.clickButtons();
         },
         
         inputDisplay(text){
             this.display.value += text;
         },
 
-        clickBotoes(){
+        doOperation(){
+            let conta = this.display.value;
+            
+            try {
+            // Problemático em um cenário real. Esse método permite execução de código inserido externamente no código.
+                conta = eval(conta);
+                this.display.value = String(conta);
+
+            } catch(error) {
+                alert('Conta inválida');
+                return;
+            }
+        },
+
+        clickButtons(){
             document.addEventListener('click', (event) => {
                 const clicado = event.target;
                 
@@ -28,6 +42,10 @@ function criaCalculadora(){
                 if(clicado.classList.contains('erase')){
                     this.eraseChar();
                 }
+
+                if(clicado.classList.contains('equals')){
+                    this.doOperation();
+                }
             });
         },
         
@@ -38,6 +56,14 @@ function criaCalculadora(){
         eraseChar(){
             this.display.value = this.display.value.slice(0, -1);
         },
+
+        // pressEnter(){
+        //     this.display.addEventListener('keyup', (event) => {
+        //         if(event.keyCode === 13){
+        //             this.doOperation();
+        //         };
+        //     });
+        // },
     };
 }
 
